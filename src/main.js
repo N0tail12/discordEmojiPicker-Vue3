d9edd87a092867af359270b7433bf6b327df7f81
N0tail12 <62517449+N0tail12@users.noreply.github.com>
2021-12-09 01:04:15 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,1 @@
-import { createApp } from 'vue'
-import App from '@/App.vue'
-import './assets/tailwind.css'
-
-createApp(App).mount('#app')
+module.exports = require('./componets/DiscordPicker.vue')
